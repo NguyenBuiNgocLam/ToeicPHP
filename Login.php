25afbaa7810b7e3b89e9c2b6a258a27639f05fb7
NguyenBuiNgocLam <152853671+NguyenBuiNgocLam@users.noreply.github.com>
2026-03-31 16:35:17 +0700

    Update Login.php

--- Login.php
+++ Login.php
@@ -1,6 +1,12 @@
 <?php
 header("Content-Type: application/json; charset=UTF-8");
-
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
 require_once 'connect.php';
 
 $conn = connectDatabase();
