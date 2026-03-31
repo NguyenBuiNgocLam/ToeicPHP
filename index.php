a01565cff8fdf0c8e8be98407ae86013886df5d0
NguyenBuiNgocLam <152853671+NguyenBuiNgocLam@users.noreply.github.com>
2026-03-31 15:48:26 +0700

    Add database connection and success message

--- index.php
+++ index.php
@@ -1,2 +1,13 @@
 <?php
-echo "Deploy PHP Render thành công!";
+require_once 'connect.php';
+
+$conn = connectDatabase();
+
+echo json_encode([
+    "success" => true,
+    "message" => "Kết nối database thành công!"
+], JSON_UNESCAPED_UNICODE);
+
+// Đóng kết nối (tùy chọn)
+mysqli_close($conn);
+?>
